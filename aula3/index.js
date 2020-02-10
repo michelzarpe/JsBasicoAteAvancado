a11c422452972accb59249093c3ab90fe3321cc8
michelzarpe <michelzarpe@gmail.com>
2020-02-10 19:08:39 -0300

    Aula3

---
+++ aula3/index.js
@@ -0,0 +1,9 @@
+//formas de listar no console do navegador...
+let vlr = 99;
+console.log("Listar na tela");
+console.warn("Atencao")
+console.error("Error");
+console.group("Grupo 1");
+console.group("Grupo 2");
+console.group(`valor -> ${vlr}`);
+console.group('valor ->',vlr);
