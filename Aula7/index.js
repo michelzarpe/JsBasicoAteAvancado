e8e5698fcef9222c8ac987ef963184f12e4b6dc6
michelzarpe <michelzarpe@gmail.com>
2020-02-10 19:58:21 -0300

    aula7

---
+++ Aula7/index.js
@@ -0,0 +1,5 @@
+// não posso declarar uma constante sem inicializar ela. 
+// também não posso colocar outro valor para ela.
+const nome = 'Michel Elimar Zarpelon';
+console.log(nome);
+console.log(typeof nome);
---
+++ aula6/index.js
@@ -0,0 +1,11 @@
+// nao pode criar variaveis com palavras reservadas.
+// variaveis tem que ter nome significativo
+// usando let
+let nomeDaVAriavel = 'Michel Zarpelon'
+console.log(`Nome: ${nomeDaVAriavel}`);
+
+/*
+var: Declara uma variável, opcionalmente, inicializando-a com um valor.
+let: Declara uma variável local de escopo do bloco, opcionalmente, inicializando-a com um valor.
+const: Declara uma constante de escopo de bloco, apenas de leitura.
+*/
