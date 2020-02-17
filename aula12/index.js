63ecc4b9bee8953d9f4c998c99e57702664688a3
michelzarpe <michelzarpe@gmail.com>
2020-02-17 20:12:38 -0300

    aula13

---
+++ aula12/index.js
@@ -0,0 +1,7 @@
+let varA ='a';
+let varB ='b';
+let varC ='c';
+
+console.log('1) ',varA,varB,varC);
+[varA,varB,varC]=[varB,varC,varA]
+console.log('2) ',varA,varB,varC);
---
+++ aula13/index.js
@@ -0,0 +1,8 @@
+//string um array de caracter
+//ou seja um texto
+// para aparecer as aspas dentro da linguagem colocar o \ de escape
+// pode ser iteraveis
+// inicia em zero 
+// termina em '\o'
+//pegar algum elemento pelo indice - string.charAt(6);
+//pegar o valor da tabela asc string.codCharAt(6);
