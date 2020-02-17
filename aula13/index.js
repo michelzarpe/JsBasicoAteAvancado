6327af433a7c45f99645da89e9f0935db88a2c46
michelzarpe <michelzarpe@gmail.com>
2020-02-17 20:25:30 -0300

    aula13

--- aula13/index.js
+++ aula13/index.js
@@ -6,3 +6,13 @@
 // termina em '\o'
 //pegar algum elemento pelo indice - string.charAt(6);
 //pegar o valor da tabela asc string.codCharAt(6);
+// quando uma palavra comeca string.indexOf('texto');
+// procurando uma palavra iniciando por un indice especifico .string.indexOf('Text',5)
+// procurar de traz pra frente string.lastIndexOf('m',4);
+// expressao regular.. string.match(/[a-z]/g)
+// replace, substitui uma pela outra string.replace('a','b'); pode usar uma expressao regular no primeiro parametro e colocar o g ele vai fazendo até terminar a string
+//fatiar uma string.. .string.slice(2,4).. inicio e fim
+//string.slice(-5,-1); pega de traz e continua.
+//string.substring(1,10); copia de inicio para o fim
+//string.split('') dividir a string por algum caracter retornando um array
+// w3school para mais informacoes;
