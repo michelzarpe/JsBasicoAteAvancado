ef0f0bb994812d70e66b31b78e1b38ec46b91064
michelzarpe <michelzarpe@gmail.com>
2020-02-23 16:11:46 -0300

    aula 14 e 15

---
+++ aula14/index.js
@@ -0,0 +1,13 @@
+//IEEE 754-2008
+console.log(0.7+0.1) // 0.799999
+console.log(Number((0.7+0.1).toFixed(2)))
+
+let num=1;
+let num1=2.5;
+let nume2 =10.13412341234
+console.log(num.toString());//string
+console.log(num.toString(2));//string binario
+//toFixed(2), duas casas decimais.s
+
+//Number.isInteger(10); // saber se é um numero inteiro ou não
+//Number.isNaN(temp) // saber se a conta é valida ou não
---
+++ aula15/index.js
@@ -0,0 +1,9 @@
+// objeto disponivel sem precisar importar Math
+//arredondamento pra baixo. numero = Math.floor(1.9283);
+//arredondamento para cima numero = Math.ceil(1.9283);
+//numero arredendado para o mais proximo. numero = Math.round(1.7262);
+//Math.max(1,2,3,4).. retorna o maior..
+//Math.min retorna o menor
+//Math.randon() de zero até 0.99999999999999999999
+//Math.round(Math.randon() * (maximo - minimo) + minimo)
+//Math.pow(2,10); dois elevado na 10
